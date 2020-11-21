39a5aa35a53491027a486ebda6b66f84eb783378
austinhallock <austinhallock@gmail.com>
2020-11-20 18:37:07 -0700

    role upsert

--- src/models/role.js
+++ src/models/role.js
@@ -8,11 +8,30 @@ export default class Role {
       query: `
         query Roles {
           roles {
-            nodes { id, name, slug }
+            nodes { id, name, slug, permissions }
           }
         }`,
       // variables: { orgId },
       pull: 'roles'
     })
+  }
+
+  upsert = ({ id, slug, name, permissions }) => {
+    return this.auth.call({
+      query: `
+        mutation RoleUpsert(
+          $id: ID!
+          $slug: String!
+          $name: String
+          $permissions: JSON
+        ) {
+          roleUpsert(id: $id, slug: $slug, name: $name, permissions: $permissions) {
+            name
+          }
+        }
+`,
+      variables: { id, slug, name, permissions },
+      pull: 'role'
+    }, { invalidateAll: true })
   }
 }
